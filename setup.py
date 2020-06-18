3a9342487595868fe7721c9fe2e721d40161c34d
Daniel Clarke <u8sand@gmail.com>
2020-06-18 18:13:02 -0400

    Fixed setup.py with git requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
+requirements = list(map(str.strip, open('requirements.txt', 'r').readlines()))
+
 setup(
   name='fairshake_assessments',
   version='0.0.0',
   url='https://github.com/maayanLab/fairshake-assessments/',
   author='Daniel J. B. Clarke',
   author_email='u8sand@gmail.com',
-  long_description=open('../README.md', 'r').read(),
+  long_description=open('README.md', 'r').read(),
   license='Apache-2.0',
-  install_requires=list(map(str.strip, open('requirements.txt', 'r').readlines())),
-  packages=find_packages(),
+  install_requires=[requirement for requirement in requirements if '://' not in requirement],
+  dependency_links=[requirement for requirement in requirements if '://' in requirement],
+  packages=find_packages(exclude=('example',)),
 )
